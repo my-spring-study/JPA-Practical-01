3bdb339af0d97be89eed931b396e52454c0d92da
Seonho Kim <sunhowold@naver.com>
2023-03-16 23:12:37 +0900

    em.persist()만으로 'id'가 생긴다.

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,9 +1,11 @@
-package jpabook.jpashop.domain.member;
+package jpabook.jpashop.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+
+import jpabook.jpashop.domain.member.Member;
 
 @Repository
 public class MemberRepository {
@@ -11,6 +13,10 @@ public class MemberRepository {
 	@PersistenceContext
 	private EntityManager em;
 
+	/**
+	 * 영속성 컨텍스트에 의해 관리될 때 id 생성(key, value)
+	 * em.persist()만 해도 id 생성
+	 */
 	public Long save(Member member) { // command 와 query를 분리하라 원칙 적용
 		em.persist(member);
 		return member.getId();
