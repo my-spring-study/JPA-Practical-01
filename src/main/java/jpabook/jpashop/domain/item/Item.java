4de77c9e08fdb21ea8583ee134664c6dd0b23820
Seonho Kim <sunhowold@naver.com>
2023-03-17 22:12:35 +0900

    상품 도메인 개발 - 비즈니스로직, repository, service

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.domain.item;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
@@ -13,14 +12,13 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
 	@Id
@@ -38,5 +36,28 @@ public abstract class Item {
 	private List<Category> categories = new ArrayList<>();
 
 	public Item() {
+	}
+
+	/* "엔티티 안에 비즈니스 로직을 넣는 것이 좋다(DDD)." */
+	//==비즈니스 로직==//
+
+	/**
+	 * stock 증가
+	 */
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * stock 감소
+	 */
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+
+		this.stockQuantity = restStock;
 	}
 }
