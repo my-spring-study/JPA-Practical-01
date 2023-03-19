11efd18ba404109ad719b5af44c691c74997ed95
Seonho Kim <sunhowold@naver.com>
2023-03-19 16:41:22 +0900

    웹 계층 개발 - 상품 주문

---
+++ src/main/java/jpabook/jpashop/controller/order/OrderController.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.controller.order;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.member.Member;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+	private final OrderService orderService;
+	private final MemberService memberService;
+	private final ItemService itemService;
+
+	@GetMapping("/order")
+	public String createForm(Model model) {
+
+		List<Member> members = memberService.findMembers();
+		List<Item> items = itemService.findItems();
+
+		model.addAttribute("members", members);
+		model.addAttribute("items", items);
+
+		return "order/orderForm";
+	}
+
+	// TODO: 회원이 여러 상품을 주문할 수 있게 변경
+	@PostMapping("/order")
+	public String order(
+		@RequestParam("memberId") Long memberId,
+		@RequestParam("itemId") Long itemId,
+		@RequestParam("count") int count
+	) {
+		// "컨트롤러에서 직접 엔티티를 찾지 않는 이유: 식별자만 넘겨주는 것이 서비스 레이어에서 할 수 있는 것이 더 많아진다."
+		// 바깥에서 member 를 넣어주면 파라미터로 받은 member 는 JPA와는 관련없는 (영속 상태가 아닌) 객체가 넘어간다.
+		orderService.order(memberId, itemId, count);
+		return "redirect:/orders";
+	}
+}
